2a82946f9f71583c4e7795e45998aa00e5a412e9
Shraya Ramani <shraya@mongodb.com>
2016-05-18 13:44:14 -0400

    EVG-941 version changes

--- version.go
+++ version.go
@@ -15,21 +15,54 @@ func getVersion(w http.ResponseWriter, r *http.Request) {
 }
 
 // getTasksForVersion sends back the list of TaskJSON documents associated with a version id.
-func getTasksForVersion(w http.ResponseWriter, r *http.Request) {
+func findTasksForVersion(versionId, name string) ([]TaskJSON, error) {
 	var jsonForTasks []TaskJSON
 	err := db.FindAllQ(collection, db.Query(bson.M{VersionIdKey: mux.Vars(r)["version_id"],
 		NameKey: mux.Vars(r)["name"]}), &jsonForTasks)
 	if err != nil {
 		if err != mgo.ErrNotFound {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			return nil, err
 		}
-		http.Error(w, "{}", http.StatusNotFound)
+		return nil, err
+	}
+
+	return jsonForTasks, err
+}
+
+// getTasksForVersion sends back the list of TaskJSON documents associated with a version id.
+func getTasksForVersion(w http.ResponseWriter, r *http.Request) {
+	jsonForTasks, err := findTasksForVersion(mux.Vars(r)["version_id"], mux.Vars(r)["name"])
+	if jsonForTasks == nil {
+		http.Error(w, http.StatusNotFound, []TaskJSON{})
 		return
 	}
-	if jsonForTasks != nil {
-		plugin.WriteJSON(w, http.StatusOK, jsonForTasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	plugin.WriteJSON(w, http.StatusNotFound, []TaskJSON{})
+	plugin.WriteJSON(w, http.StatusOK, jsonForTasks)
+	return
+}
+
+// getTasksForLatestVersion sends back the TaskJSON data associated with the latest version.
+func getTasksForLatestVersion(w http.ResponseWriter, r *http.Request) {
+	var jsonForTasks []TaskJSON
+	project := mux.Vars(r)["project_id"]
+	name := mux.Vars(r)["name"]
+
+	jsonTask := db.FindOneQ(collection, db.Query(bson.M{NameKey: name,
+		ProjectIdKey: project}).Sort([]string{"-" + RevisionOrderNumberKey}).WithFields(VersionIdKey))
+	if jsonTask.VersionId == "" {
+		http.Error(w, http.StatusNotFound, []TaskJSON{})
+	}
+	jsonTasks, err := findTasksForVersion(jsonTask.VersionId, name)
+	if jsonForTasks == nil {
+		http.Error(w, http.StatusNotFound, []TaskJSON{})
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	plugin.WriteJSON(w, http.StatusOK, jsonForTasks)
 }
